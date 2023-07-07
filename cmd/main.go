6e74888ec2e9ede796b7200d30b2483ff6fa6e87
halalala222 <1741196223@qq.com>
2023-07-07 20:38:02 +0800

    feat:gin run

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"github.com/halalala222/cursor-pagination-redis-cache-sample/internal/api/sample"
+	"log"
+)
 
+func main() {
+	engine := sample.Run()
+	if err := engine.Run(":8088"); err != nil {
+		log.Fatal(err)
+	}
 }
